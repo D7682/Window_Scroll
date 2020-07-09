30d95edc0418ec1f0d79942d4646da7aa01d394a
D7682 <danielgomez7682@gmail.com>
2020-07-09 11:46:26 -0500

    2nd

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-window.onscroll = () => {
-    console.log(window.pageYOffset);
+let button = document.getElementById('button');
+
+onscroll = () => {
+    console.log(pageYOffset);
+}
+
+
+
+button.onmouseover = () => {
+    document.body.style.background = 'red';
+}
+
+button.onmouseout = () => {
+    document.body.style.background = 'blue';
+}
+
+button.onclick = () => {
+    console.log('hello');
 }
